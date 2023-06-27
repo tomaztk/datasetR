9b5e4e48177eeeaae0bed3eeeb92c4f710d11e7f
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-06-27 06:33:31 +0200

    update

--- R/dsR.R
+++ R/dsR.R
@@ -7,7 +7,9 @@
 #' @examples
 #' getType(1);
 #' getType(3);
+#'
 #' @export
+#'
 getType <- function(var_enum_enter=1) {
   nam_val<- as.data.frame(substr(capture.output(str(set_of_val)),1,10))
   nam_val <- as.data.frame(nam_val[2:dim(nam_val)[1],])
@@ -21,42 +23,20 @@ getType <- function(var_enum_enter=1) {
   return(tip)
 }
 
-#' #'  Sample data
-#' #' @examples
-#' #' data(set_of_val)
-#' #' @export
-#' set_of_val <- list(
-#'   ms = c("red", "green", "blue", "black", "magenta"),
-#'   ms = c("Europe", "Australia", "Asia", "Africa", "America", "Antarctica"),
-#'   ms = c(20123,3502,102023, 1000, 54903),
-#'   ms = c("Ljubljana","Antananarivo","Taipeh","Tokyo","Canberra","Seattle","Asuncion","Windhoek"),
-#'   od = c("XS", "S", "M", "L", "XL"),
-#'   od = c(1,2,3,4,5,6),
-#'   li = c("Agree", "Neutral", "Disagree"),
-#'   li = c("Strongly Agree", "Agree", "Neutral", "Disagree", "Strongly Disagree"),
-#'   li = c("Strongly Agree", "Agree", "Somewhat Agree", "Neutral","Somewhat Disagree", "Disagree", "Strongly Disagree"),
-#'   ii = 1:1000,
-#'   ii = 9000:20120,
-#'   bo = c(TRUE,FALSE),
-#'   bi = c("Yes", "No"),
-#'   bi = c("1", "0"),
-#'   le = LETTERS,
-#'   le = letters,
-#'   gu = sapply(seq_len(1000), function(x) {system("uuidgen", intern=T)}),
-#'   te = sapply(seq_len(1000), function(x) {sample(c(-20:35),1, replace=TRUE)}),
-#'   te = sapply(seq_len(1000), function(x) {sample(c(1:130),1, replace=TRUE)}),
-#'   mo = sapply(seq_len(1000), function(x) {sample(c(10:10000),1, replace=TRUE)})
-#' )
 
 #' Get the starting set of values for constructing the dataframe
 #'
-#' @returns list.
+#' @param list.
 #' @examples
 #' getValues()
+#'
+#' @return This function returns a data frame.
+#'
 #' @export
+#'
 getValues <- function(){
   #ms = nominal ; mo = ordinal; li = likert; bo = boolean; bi = binary; le = letters; gu = GUID; te = temperature; mo = money
-  set_of_val <<- list(
+  set_of_val <- list(
     ms = c("red", "green", "blue", "black", "magenta"),
     ms = c("Europe", "Australia", "Asia", "Africa", "America", "Antarctica"),
     ms = c(20123,3502,102023, 1000, 54903),
@@ -88,7 +68,9 @@ getValues <- function(){
 #' @return dataframe
 #' @examples
 #' dsR(vr="ms:3;bi:4;ii:1", nr=100);
+#'
 #' @export
+#'
 dsR <- function(vr="ms:3;bi:4;ii:1", nr=100){
   library(dplyr)
   df <- NULL
