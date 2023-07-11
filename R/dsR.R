700c143e2d2ad7009fce32b04009522a608f3b19
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-07-11 05:49:56 +0200

    update function addmissing

--- R/dsR.R
+++ R/dsR.R
@@ -174,21 +174,24 @@ dsR <- function(vr="ms:3;bi:4;ii:1", nr=100){
 #' Function for replacing values with missing NA value for a given dataframe and variable
 #'
 #' @param df Name of the dataframe
-#' @param var Name of the variable
+#' @param col_name Name of the variable
 #' @param pct Percent of missing values
 #'
 #' @return dataframe
 #' @examples
-#' addMissingValues(df=df, var=var, pc=10);
+#' addMissingValues(df=df, col_name=var, pc=10);
 #'
 #' @export
 #'
-addMissingValues <- function(df,var,pc=10) {
-  stopifnot(nrow(df)>0)
+addMissingValues <- function(df_name,col_name,pc=10) {
+  stopifnot(nrow(df_name)>0)
   #ar <- match.call()
   #print(df[[ar$col_name]])
-  df$var1[df$var1 %in% sample(df$var1, (pc/100)*nrow(df), replace=FALSE)] <<- NA
+  col_name <- deparse(substitute(col_name))
+  df_name[[col_name]][df_name[[col_name]] %in% sample(df_name[[col_name]], (pc/100)*nrow(df_name), replace=FALSE)] <- NA
+  df_name[[col_name]]
 }
+
 
 
 
