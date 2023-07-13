205d5d874f17bf56eacbc41e634348c7646705a8
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2023-07-13 22:41:12 +0200

    update addMissingValues function

--- R/dsR.R
+++ R/dsR.R
@@ -186,11 +186,15 @@ dsR <- function(vr="ms:3;bi:4;ii:1", nr=100){
 addMissingValues <- function(df_name,col_name,pc=10) {
   stopifnot(nrow(df_name)>0)
   col_name <- deparse(substitute(col_name))
-  df_name[[col_name]][df_name[[col_name]] %in% sample(df_name[[col_name]], (pc/100)*nrow(df_name), replace=FALSE)] <- NA
+  #df_name[[col_name]][df_name[[col_name]] %in% sample(df_name[[col_name]], (pc/100)*nrow(df_name), replace=FALSE)] <- NA
+  #df_name[[col_name]]
+  nof_vals <- round((pc/100)*nrow(df_name))
+  positions <- 1:nrow(df_name)
+  rand_pos <- sample(positions, nof_vals, replace=FALSE)
+
+  for (i in 1:length(rand_pos)){
+    df_name[[col_name]][rand_pos[i]] <- NA
+  }
   df_name[[col_name]]
 }
-
-
-
-
 
